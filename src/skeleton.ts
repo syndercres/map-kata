2052cff1380d0e12de2d04cc605b186681fdebbe
syndercombecreswell <syndercombecreswell@gmail.com>
2022-11-10 10:38:07 +0000

    pseudocode

--- src/skeleton.ts
+++ src/skeleton.ts
@@ -1,3 +1,22 @@
+/*
+  function filter
+  parameters: inputArr (any), predicateFn (function)
+  returns returnArr (any)
+
+  returnArr (empty array)
+  
+  for each element of inputArr
+    if predicate function returns true
+      push element to empty array
+    end if
+  end for
+*/
+
+
+
+
+
+
 /**
  * Adds together two numbers
  * @param a - the first number to add
